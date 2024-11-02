9b63662b3d1027a46e6f4389ba3f0f1952690606
Giancarlo Fernandez <giancarlo.fernandez@outlook.com>
2024-11-01 19:00:55 -0500

    updating files

---
+++ demoApiExt/src/main/java/com/api_externas_demo/demoApiExt/DemoApiExtApplication.java
@@ -0,0 +1,13 @@
+package com.api_externas_demo.demoApiExt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiExtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiExtApplication.class, args);
+	}
+
+}
---
+++ demoApiExt/src/test/java/com/api_externas_demo/demoApiExt/DemoApiExtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_externas_demo.demoApiExt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiExtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
