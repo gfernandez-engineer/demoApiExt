689a6efa0479695f9b8f6d396c93de2a1f68c297
Giancarlo Fernandez <giancarlo.fernandez@outlook.com>
2024-11-04 19:44:42 -0500

    updating files

---
+++ demoApiExt/src/main/java/com/api_externas_demo/demoApiExt/aggregates/constants/Constans.java
@@ -0,0 +1,8 @@
+package com.api_externas_demo.demoApiExt.aggregates.constants;
+
+public class Constans {
+    public static final String USER_CREATED = "GFERNANDEZ";
+    public static final String BEARE = "Bearer ";
+    public static final int ESTADO_ACTIVO=1;
+    public static final String BASE_URL = "https://api.apis.net.pe";
+}
